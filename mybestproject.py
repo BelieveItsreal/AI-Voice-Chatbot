cd19dbc4b5dabfe13b71d7c0865f52ad646afb8e
Atanu <malatiexe@gmail.com>
2023-05-14 05:27:15 +0530

    Update mybestproject.py

--- mybestproject.py
+++ mybestproject.py
@@ -4,6 +4,8 @@ import pyttsx3
 import vosk
 import pyaudio
 import json
+import tkinter as tk
+from PIL import Image, ImageTk
 from EdgeGPT import Chatbot, ConversationStyle
 
 # Initialize pyttsx3 for text-to-speech
@@ -43,7 +45,7 @@ async def main():
             for message in response["item"]["messages"]:
                 if message["author"] == "bot":
                     bot_response = message["text"]
-                    bot_response2 = re.sub('\[\^\d+\^\]', '', bot_response)
+                    bot_response2 = re.sub('\[\^\d+\^\]', '', bot_response)   #type: ignore
                     print("bot's response:", bot_response2)
                     text_speech.say(bot_response2)
                     text_speech.runAndWait()
@@ -51,18 +53,45 @@ async def main():
 
     await bot.close()
 
-assistant_is_on = True
-while assistant_is_on:
-    user_choice = input("\ntype 'Y' to ask question and 'exit' to exit the bot: ").lower()
-    if user_choice == "y":
-        asyncio.run(main())
-    elif user_choice == "exit":
-        print("Good bye sir. If u Have any other question feel free to ask me again")
-        text_speech.say("Good bye sir. If u Have any other question feel free to ask me again")
-        text_speech.runAndWait()
-        assistant_is_on = False
-    else:
-        print("you have enetered a wrong input try again, type 'Y' to ask question and 'exit' to exit the bot")
+def exit_program():
+    print("Good bye sir. If you have any other question feel free to ask me again")
+    text_speech.say("Good bye sir. If you have any other question feel free to ask me again")
+    text_speech.runAndWait()
+    window.quit()
+
+# create the tkinter window
+window = tk.Tk()
+window.title("Chatbot Assistant")
+window.geometry("500x400")
+
+# Set the background image
+bg_image = ImageTk.PhotoImage(Image.open("D:\\coding\\python_in_hole\\python code\\bot png\\back.JPG"))
+bg_label = tk.Label(window, image=bg_image)
+bg_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+# create the UI elements
+title_label = tk.Label(window, text="Welcome to Chatbot Assistant", font=("Arial", 20))
+title_label.pack(pady=20)
+
+prompt_label = tk.Label(window, text="Tap mic to ask question:", font=("Arial", 14))
+prompt_label.pack()
+
+mic_image = ImageTk.PhotoImage(Image.open("D:\\coding\\python_in_hole\\python code\\bot png\\mic.png").resize((50,50)))
+ask_button = tk.Button(window, image=mic_image, command=lambda: asyncio.run(main()))
+ask_button.pack(pady=10)
+
+mic_label = tk.Label(window, text="Tap Mic To Ask Question", font=("Arial", 12))
+mic_label.pack()
+
+exit_image = ImageTk.PhotoImage(Image.open("D:\\coding\\python_in_hole\\python code\\bot png\\exit.png").resize((50,50)))
+exit_button = tk.Button(window, image=exit_image, command=exit_program)
+exit_button.pack(pady=30)
+
+exit_label = tk.Label(window, text="Tap Icon To Close Bot", font=("Arial", 12))
+exit_label.pack()
+
+# run the tkinter event loop
+window.mainloop()
 
 # Clean up
 stream.stop_stream()
